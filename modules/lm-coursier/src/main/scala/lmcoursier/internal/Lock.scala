5ab0e9b5578dc5f5269caef374e70c76d4d681b6
Øyvind Raddum Berg <oyvind.raddum.berg@nrk.no>
2020-05-29 12:08:39 +0200

    Disable lock

--- modules/lm-coursier/src/main/scala/lmcoursier/internal/Lock.scala
+++ modules/lm-coursier/src/main/scala/lmcoursier/internal/Lock.scala
@@ -5,6 +5,5 @@ private[lmcoursier] object Lock {
   // Wrap blocks downloading stuff (resolution / artifact downloads) in lock.synchronized.
   // Downloads are already parallel, no need to parallelize further, and this results in
   // a clearer output.
-  val lock = new Object
-
+  def lock = new Object
 }
